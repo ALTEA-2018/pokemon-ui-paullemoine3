6701f1a9ddd385c09b2666f63e0ce9db47a3da1d
Paul Lemoine <paul.lemoine.1@gmail.com>
2019-03-04 10:10:47 +0100

    add management of local

--- src/main/java/com/miage/altea/tp/pokemon_ui/controller/PokemonTypeController.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/controller/PokemonTypeController.java
@@ -5,11 +5,9 @@ import com.miage.altea.tp.pokemon_ui.pokemonTypes.service.PokemonTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Collections;
-import java.util.Comparator;
+
 import java.util.List;
 
 @Controller
--- src/main/java/com/miage/altea/tp/pokemon_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
@@ -4,6 +4,9 @@ import com.miage.altea.tp.pokemon_ui.config.RestConfiguration;
 import com.miage.altea.tp.pokemon_ui.pokemonTypes.bo.PokemonType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,8 +25,11 @@ public class PokemonTypeServiceImpl implements PokemonTypeService {
 
     @Override
     public List<PokemonType> listPokemonsTypes() {
+        var headers = new HttpHeaders();
+        headers.setAcceptLanguageAsLocales(List.of(LocaleContextHolder.getLocale()));
+        var httpRequest = new HttpEntity<>(headers);
         PokemonType[] listPoke;
-        listPoke = rT.getForObject(this.pokemonServiceUrl+this.base_url, PokemonType[].class);
+        listPoke = rT.getForObject(this.pokemonServiceUrl+this.base_url, PokemonType[].class, httpRequest);
         List<PokemonType> listPT = Arrays.asList(listPoke);
         return listPT;
     }
